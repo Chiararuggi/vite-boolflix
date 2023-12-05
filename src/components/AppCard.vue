f47f9b4732480cc8f95a80aad7b12099735a4207
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-12-05 18:00:37 +0100

    add img when searching for movies

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -13,11 +13,12 @@ export default {
     <p>{{ info.original_title }}</p>
     <p>{{ info.vote_count }}</p>
     <p>{{ info.original_language }}</p>
+    <img :src="`https://image.tmdb.org/t/p/w342/${info.poster_path}`" />
   </div>
 </template>
 
 <style scoped>
 .card {
-  color:black;
+  color: black;
 }
 </style>
